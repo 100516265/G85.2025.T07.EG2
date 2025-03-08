d8de2cd09f30555b3bb5f70da5de7d63448dbfb6
100516265 <100516265@alumnos.uc3m.es>
2025-03-08 19:37:53 +0100

    DEFINICIONES METODOS

--- src/main/python/uc3m_money/account_manager.py
+++ src/main/python/uc3m_money/account_manager.py
@@ -1,4 +1,7 @@
-"""Module """
+"""Module
+from .account_management_exception import AccountManagementException
+from .transfer_request import TransferRequest
+"""
 
 class AccountManager:
     """Class for providing the methods for managing the orders"""
@@ -6,7 +9,52 @@ class AccountManager:
         pass
 
     @staticmethod
-    def validate_iban(iban: str):
-        """RETURNs TRUE IF THE IBAN RECEIVED IS VALID SPANISH IBAN,
-        OR FALSE IN OTHER CASE"""
-        return True
+    def validate_iban(iban: str) ->bool:
+        """
+        Validates whether a given IBAN is correctly formatted
+        and has valid control digits
+        :param iban: The IBAN to validate
+        :return: True if the IBAN is valid
+        """
+
+        if(not isinstance(iban, str)
+            or not iban.startswith("ES")
+            or len(iban)!=24
+            or not iban[2:].isdigit()):
+            return False
+
+        control_digits = int(iban[2:4])
+        iban_number = int(iban[4:] + "142800") % 97
+        calculated_control_digits = 98 - iban_number
+
+        return control_digits == calculated_control_digits
+
+
+    @staticmethod
+    def validate_concept(concept: str) ->bool:
+
+        if(not isinstance(concept, str)
+            or not (10<= len(concept) <= 30)):
+            return False
+
+
+    @staticmethod
+    def validate_type(type: str) ->bool:
+
+        if not isinstance(type, str):
+            return False
+        if type == "ORDINARY" or "URGENT" or "INMEDIATE":
+            return True
+
+    @staticmethod
+    def validate_date(date: str) -> bool:
+
+        if not isinstance(date, str):
+            return False
+
+    @staticmethod
+    def validate_amount(amount: str) -> bool:
+
+        if (not isinstance(amount, float))\
+            or not (10.00 <= amount <= 10000.00):
+            return False
