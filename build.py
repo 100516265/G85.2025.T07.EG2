4a9dd352f907daf3d0f74115a31878439067f2c5
100557973 <100557973@alumnos.uc3m.es>
2025-03-24 15:33:47 +0100

    UPDATE: FIX BUILD

--- build.py
+++ build.py
@@ -1,4 +1,6 @@
+"""Configuration file for the PyBuilder build tool."""
 #   -*- coding: utf-8 -*-
+# pylint: skip-file
 from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
@@ -7,9 +9,8 @@ use_plugin("python.coverage")
 use_plugin("python.flake8")
 use_plugin("python.distutils")
 
-
-NAME = "G85.2025.T07.GE2"
-DEFAULT_TASK = "publish"
+name = "G85.2025.T07.GE2.bis"
+default_task = "publish"
 
 
 @init
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #   -*- coding: utf-8 -*-
+# pylint: skip-file
 #
 #   This file is part of PyBuilder
 #
